87c5a1451c3ed9a7a260d336e5b2d6bd85e9bc6e
Badin1 <66058511+Badin1@users.noreply.github.com>
2021-01-07 20:47:38 +0300

    Ошибка в задании 11 (блок для групп)

--- test/test_group_edit.py
+++ test/test_group_edit.py
@@ -5,13 +5,19 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test4"))
-    app.group.test_edit(Group(name="new test1"))
+    old_groups = app.group.get_group_list()
+    app.group.test_edit(Group(name="new test1", footer="new test"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test4"))
+    old_groups = app.group.get_group_list()
     app.group.test_edit(Group(header="new test1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 
