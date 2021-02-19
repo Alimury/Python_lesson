c8eca67f2aa043a74b365583436ae1b20cf69da4
Badin1 <66058511+Badin1@users.noreply.github.com>
2021-02-19 17:07:25 +0300

    Общий сценарий для всех тестов в стиле Bdd для запуска через Jenkins

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -1,4 +1,5 @@
 from pytest_bdd import scenario
+from bdd.contact_steps import *
 from bdd.group_steps import *
 
 
@@ -14,3 +15,15 @@ def test_delete_group():
 def test_edit_group():
     pass
 
+@scenario("contact.feature", "Add new contact")
+def test_add_new_contact():
+    pass
+
+
+@scenario("contact.feature", "Delete a contact")
+def test_delete_contact():
+    pass
+
+@scenario("contact.feature", "Modification a contact")
+def test_edit_contact():
+    pass
